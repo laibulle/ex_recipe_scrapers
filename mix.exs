c8638e5e8ec030ce95847a5a2247408be50c7ba6
Guillaume Bailleul <laibulle@gmail.com>
2020-05-23 20:45:44 +0200

    chore: update package info

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule RecipeScrapers.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      compilers: [:elixir_make] ++ Mix.compilers()
+      compilers: [:elixir_make] ++ Mix.compilers(),
+      package: package(),
+      description: "Wrapper of the python library RecipeScrapers"
     ]
   end
 
@@ -25,6 +27,18 @@ defmodule RecipeScrapers.MixProject do
     [
       {:poison, "~> 3.1"},
       {:elixir_make, "~> 0.6.0", runtime: false}
+    ]
+  end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Guillaume Bailleul<laibulle@gmail.com>"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/laibulle/ex_recipe_scrapers"
+      }
     ]
   end
 end
