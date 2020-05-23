cf0dfb9c9b7e70af0ead315cd7872184e7ee1d33
Guillaume Bailleul <laibulle@gmail.com>
2020-05-23 21:31:56 +0200

    fix: release issue

--- lib/recipe_scrapers.ex
+++ lib/recipe_scrapers.ex
@@ -4,14 +4,21 @@ defmodule RecipeScrapers do
     defstruct [:name, :steps, :yields, :total_time, :ingredients, :image]
   end
 
-  @base_dir Application.app_dir(:recipe_scrapers, "priv/recipe_scrapers")
+  def base_path() do
+    priv_dir = "priv/recipe_scrapers"
+
+    case Process.whereis(Mix.State) do
+      nil -> :code.priv_dir(:recipe_scrapers) |> Path.join(priv_dir)
+      _ -> Application.app_dir(:recipe_scrapers, priv_dir)
+    end
+  end
 
   def fetch(url) do
     {res, _} =
       System.cmd(
-        @base_dir <> "/.venv/bin/python3",
+        base_path() |> Path.join("/.venv/bin/python3"),
         ["scrapers.py", url],
-        cd: @base_dir
+        cd: base_path()
       )
 
     Poison.decode(res, as: %Recipe{})
